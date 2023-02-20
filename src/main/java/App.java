9d7561cd54a3be8a2cdac91db1d80c356e4014cb
Анна <pannochk@mail.ru>
2023-02-20 15:26:52 +0700

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+        System.out.println("Переменные bean1 и bean2 ссылаются на один и тот же объект? " + (bean1==bean2));
+        System.out.println("Переменные beanCat1 и beanCat2 ссылаются на один и тот же объект? " + (beanCat1==beanCat2));
     }
 }
